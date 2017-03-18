66a4185fa8ffbec94b27123c8bfbe08d77765d9a
Jimbo <marquardtjames@gmail.com>
2017-03-18 17:36:57 -0500

    svg groups for each team rather than just circles.  groups contain circles and text labels. should eventually contain stat pop-ups

--- scatter/scatter.js
+++ scatter/scatter.js
@@ -19,25 +19,26 @@ var x = d3.scale.linear()
 var y = d3.scale.linear()
     .range([height, 0]);
 
+var radiusScale = d3.scale.linear().range([5,20])
+
+
 //set up the axis functions, to be called on svg groups 
 var xAxis = d3.svg.axis()
     .ticks(6)
     .scale(x)
     .tickSize(-height)
-    //.tickSize(4)
     .orient("bottom");
 
 var yAxis = d3.svg.axis()
     .ticks(6)
     .scale(y)
-    //.ticks(6)
     .tickSize(-width)
-    //.tickSize(4)
     .orient("right")
     ;
 
 x_var = document.querySelector("#x-axis-select").value;
 y_var = document.querySelector("#y-axis-select").value;
+radius_var = document.querySelector("#radius-select").value;
 
 
 //make the actual svg, sizing it, and then add a group, which will act as our main selection.  the group has been translated (moved) to the origin plus the margin
@@ -57,21 +58,31 @@ svg.append("clipPath")
 	    .attr("height", height);
 
 
-
+// get the data and use it to create the chart
 d3.json("team_stats.json", function(err, data) {
 	if (err) throw err;
 	chart_data = data;
 	console.dir(chart_data[0]);
 	console.log(Object.keys(chart_data[0]));
 
-	x.domain([
-		d3.min(chart_data, function(d) { return d[x_var]; }),
-	 	d3.max(chart_data, function(d) { return d[x_var]; })
-	 ]).nice();
-	y.domain([
-		d3.min(chart_data, function(d) { return d[y_var]; }),
-		d3.max(chart_data, function(d) { return d[y_var]; })
-	]).nice();
+	// function for setting up axes based on new data or new selected axis variable
+	function axisDomains(){
+		x.domain([
+			d3.min(chart_data, function(d) { return d[x_var]; }),
+		 	d3.max(chart_data, function(d) { return d[x_var]; })
+		 ]).nice();
+		y.domain([
+			d3.min(chart_data, function(d) { return d[y_var]; }),
+			d3.max(chart_data, function(d) { return d[y_var]; })
+		]).nice();
+		radiusScale.domain([
+			d3.min(chart_data, function(d) { return d[radius_var]; }),
+			d3.max(chart_data, function(d) { return d[radius_var]; })
+		]).nice();
+
+	}
+	// initialize axes
+	axisDomains();
 
 	//svg
 	  //.datum(data)// TA DA!!! Here we bind the data to the main svg group (g)
@@ -80,20 +91,39 @@ d3.json("team_stats.json", function(err, data) {
 	function circleHover(d) {
 		var mouse_coords = d3.mouse(this);
 		console.log(mouse_coords)
-		d3.select(this).style("stroke","red");
-		var hoverInfo = document.querySelector(".hover-info")
-		hoverInfo.innerHTML = d.TEAM_NAME;
-		hoverInfo.style.top = mouse_coords[1]+margin.top+"px";
-		hoverInfo.style.left = mouse_coords[0]+margin.left+"px";
+		d3.select(this).style("stroke","red")
+		d3.selectAll(".team-node text").style("display","none");
+		d3.select(this.parentNode).select("text").style("display","inline");
 	}
 
 	function circleUnhover(d) {
-		d3.select(this).style("stroke","#222");
+		d3.select(this).style("stroke","#055");
+		//d3.select(this.parentNode).select("text").style("display","none");
 	}
 
+	teamNodes = svg.selectAll(".team-node").data(chart_data);
 
-	circles = svg.selectAll("circle").data(chart_data);
+	teamNodes.enter().append("g").attr("class","team-node");
 
+	teamNodes.append("circle")
+		.attr("cx",function(d,i){return(x(d[x_var]))})
+		.attr("cy",function(d,i){return(y(d[y_var]))})
+		.attr("r",function(d,i){return(10)})
+		.style("fill","none").style("stroke-width",2)
+		.on("mouseover", circleHover)
+		.on("mouseleave", circleUnhover);
+
+	teamNodes.append("text")
+		.attr("class", "team-label")
+		.attr("x",function(d,i){return(x(d[x_var]))})
+		.attr("y",function(d,i){return(y(d[y_var]))})
+		.style("fill", "red")
+		.style("display", "none")
+		.text(function(d){return d.TEAM_NAME});
+		
+	teamNodes.exit().remove();
+
+/*
 	circles.enter().append("circle")
 		.attr("cx",function(d,i){return(x(d[x_var]))})
 		.attr("cy",function(d,i){return(y(d[y_var]))})
@@ -103,7 +133,7 @@ d3.json("team_stats.json", function(err, data) {
 		.on("mouseleave", circleUnhover);
 
     circles.exit().remove();
-
+*/
 
 
 	// add the y axis
@@ -140,7 +170,8 @@ d3.json("team_stats.json", function(err, data) {
 	function adjustChart() {
 		x_var = document.querySelector("#x-axis-select").value;
 		y_var = document.querySelector("#y-axis-select").value;
-
+		radius_var = document.querySelector("#radius-select").value;
+		/*
 		x.domain([
 			d3.min(chart_data, function(d) { return d[x_var]; }),
 			d3.max(chart_data, function(d) { return d[x_var]; })
@@ -149,7 +180,8 @@ d3.json("team_stats.json", function(err, data) {
 			d3.min(chart_data, function(d) { return d[y_var]; }),
 			d3.max(chart_data, function(d) { return d[y_var]; })
 		]).nice();
-
+		*/
+		axisDomains();
 		svg.select(".y-label")
 			.text(y_var);
 		svg.select(".x-label")
@@ -162,14 +194,14 @@ d3.json("team_stats.json", function(err, data) {
 		  .call(xAxis);
 
 
-		var t = circles.transition().duration(750);
-		t
+		var t = teamNodes.transition().duration(750);
+		t.selectAll("circle")
 			.attr("cx",function(d,i){return(x(d[x_var]))})
 			.attr("cy",function(d,i){return(y(d[y_var]))})
-			.attr("r",function(d,i){return(10)})
-			.style("fill","none")
-			.style("stroke",function(d,i){return("#222");})
-			.style("stroke-width",2)
+			.attr("r",function(d,i){return(radiusScale(d[radius_var]))})
+		t.selectAll("text")
+			.attr("x",function(d,i){return(x(d[x_var]))})
+			.attr("y",function(d,i){return(y(d[y_var]))})
 	}// end of adjustChart
 
 	adjustChart();
@@ -177,6 +209,7 @@ d3.json("team_stats.json", function(err, data) {
 	// adjust chart when user changes axis variables
 	document.querySelector("#x-axis-select").onchange=adjustChart;
 	document.querySelector("#y-axis-select").onchange=adjustChart;
+	document.querySelector("#radius-select").onchange=adjustChart;
 
 });
 
